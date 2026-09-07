8b0fab9f0e187077cea961ebeb5bfed28d3fbe8d
esmaill1 <moh12345678912mmo@gmail.com>
2026-09-07 19:41:28 +0200

    file structure update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { LanguageProvider } from './i18n'
+import { LanguageProvider } from './i18n.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
